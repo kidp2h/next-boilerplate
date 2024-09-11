a2f53865fed8e6b06b6476f4a3864edb02890048
kidp2h <nthinh.dev@gmail.com>
2024-09-11 22:30:59 +0700

    feat: first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tailwind from 'eslint-plugin-tailwindcss';
+
+export default antfu(
+  {
+    react: true,
+    typescript: true,
+
+    lessOpinionated: true,
+    isInEditor: false,
+
+    stylistic: {
+      semi: true,
+    },
+
+    formatters: {
+      css: true,
+    },
+
+    ignores: [
+      'next-env.d.ts',
+    ],
+  },
+  ...tailwind.configs['flat/recommended'],
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    files: [
+      '**/*.spec.ts',
+      '**/*.e2e.ts',
+    ],
+  },
+  {
+    rules: {
+      'import/order': 'off', // Avoid conflicts with `simple-import-sort` plugin
+      'sort-imports': 'off', // Avoid conflicts with `simple-import-sort` plugin
+      'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+      'react/prefer-destructuring-assignment': 'off',
+      'node/prefer-global/process': 'off', // Allow using `process.env`
+
+    },
+  },
+);
---
+++ src/tests/example.test.ts
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom/vitest';
+
+import { afterEach, describe, expect, it } from 'vitest';
+
+describe('example', () => {
+  afterEach(() => {
+  });
+
+  it('should work', () => {
+    expect(true).toBe(true);
+  });
+});
